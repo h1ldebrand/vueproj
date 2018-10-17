5b4b6a4eaa38f23c77edcd734379629ed6f53b8b
h1ldebrand <semenchuk@flabers.com.ua>
2018-10-17 18:15:14 +0300

    add component Car

---
+++ src/Car.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="car">
+        <h3>Name: {{ carName }}</h3>
+        <p>Year: {{ carYear }}</p>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        // props: ['carName', 'carYear'],
+        props: {
+            carName: {
+                type: String,
+                required: true,
+                default: "Default name"
+            },
+            carYear: Number
+        },
+        data() {
+            return {
+
+            }
+        }
+    }
+
+</script>
+
+<style>
+.car {
+    border: 1px solid black;
+}
+.car h3{
+    margin-bottom: 5px;
+}
+</style>
