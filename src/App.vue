92c617a728ba0d9dda6a60bcd15edc4ff999b162
h1ldebrand <vdh1ldebrand@i.ua>
2018-11-01 00:09:06 +0200

    vuerouter video 6

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="container">
+      <ul class="nav">
+          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
+              <a class="nav-link" >Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" to="/cars" active-class="active">
+              <a class="nav-link">Cars</a>
+          </router-link>
+
+          <router-link
+                  tag="li"
+                  class="nav-item"
+                  active-class="active"
+                  v-for="car in 5"
+                  :to="'/car/' + car"
+                  :key="car"
+          >
+              <a class="nav-link">Car {{ car }}</a>
+          </router-link>
+      </ul>
       <router-view></router-view>
   </div>
 </template>
@@ -12,5 +31,8 @@ export default {
 </script>
 
 <style scoped>
+    .nav-item.active .nav-link{
+        color: blueviolet;
+    }
 
 </style>
