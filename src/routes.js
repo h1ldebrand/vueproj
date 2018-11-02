d85901022bb8231684fc1fe3428a60dae60b4cba
h1ldebrand <semenchuk@flabers.com.ua>
2018-11-02 12:55:57 +0200

    video 10

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import Cars from './pages/Cars';
 import Car from './pages/Car';
 import CarFull from './pages/CarFull';
+import ErrorCmp from './pages/Error'
 
 export default new VueRouter({
     routes: [
@@ -24,7 +25,31 @@ export default new VueRouter({
                     name: 'carFull'
                 }
             ]
+        },
+        {
+            path: '/none',
+            redirect: '/cars'
+        },
+        {
+            path: '*',
+            component: ErrorCmp
         }
     ],
-    mode: 'history'
+    mode: 'history',
+    scrollBehavior(to, from, savePosition){
+
+        if(savePosition){
+            return savePosition;
+        }
+
+        if(to.hash){
+            return {selector: to.hash}
+        }
+
+        return {
+            x: 0,
+            y: 500
+        }
+
+    }
 })
