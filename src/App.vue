870776b2ce6929986f2ed4174cc6812b12706df2
h1ldebrand <vdh1ldebrand@i.ua>
2018-11-04 18:22:15 +0200

    vur-resourse watched

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,76 @@
 <template>
-  <div class="container">
-      <ul class="nav">
-          <router-link tag="li" class="nav-item" exact to="/" active-class="active">
-              <a class="nav-link" >Home</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" to="/cars" active-class="active">
-              <a class="nav-link">Cars</a>
-          </router-link>
+    <div class="container pt-2">
+        <div class="form-group">
+            <label for="name">Car name</label>
+            <input type="text" class="form-control" v-model="carName">
+        </div>
 
-          <router-link
-                  tag="li"
-                  class="nav-item"
-                  active-class="active"
-                  v-for="car in 5"
-                  :to="'/car/' + car"
-                  :key="car"
-          >
-              <a class="nav-link">Car {{ car }}</a>
-          </router-link>
-      </ul>
-      <router-view></router-view>
-  </div>
+        <div class="form-group">
+            <label for="year">Car year</label>
+            <input type="text" class="form-control" id="year" v-model="carYear">
+        </div>
+
+        <button class="btn btn-success" @click="createCar">Create car</button>
+        <button class="btn btn-primary" @click="loadCars">Load cars</button>
+
+        <hr>
+
+        <ul class="list-group">
+            <li
+                class="list-group-item"
+                v-for="car of cars"
+                :key="car.id"
+            >
+                <strong>{{ car.name }}</strong> - {{ car.year }}
+            </li>
+        </ul>
+
+    </div>
 </template>
 
 <script>
 
 export default {
-
+    data(){
+        return {
+            carName: '',
+            carYear: 2018,
+            cars: [],
+            resource: null
+        }
+    },
+    methods: {
+        createCar(){
+            const car = {
+                name: this.carName,
+                year: this.carYear
+            }
+            // this.$http.post('http://localhost:3000/cars', car)
+            //     .then(response => {
+            //         return response.json()
+            //     })
+            //     .then(newCar => {
+            //         console.log(newCar)
+            //     })
+            this.resource.save({}, car)
+        },
+        loadCars(){
+            // this.$http.get('http://localhost:3000/cars')
+            this.resource.get()
+                .then(response => {
+                    return response.json()
+                })
+                .then(cars => {
+                    this.cars = cars;
+                })
+        }
+    },
+    created(){
+        this.resource = this.$resource('cars')
+    }
 }
 </script>
 
 <style scoped>
-    .nav-item.active .nav-link{
-        color: blueviolet;
-    }
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import router from './routes'
-
+import VueResourse from 'vue-resource';
 import App from './App.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueResourse);
+
+Vue.http.options.root = 'http://localhost:3000/';
+
+Vue.http.interceptors.push(request => {
+    request.headers.set('Auth', 'RAND TOKEN ' + Math.random())
+})
 
 new Vue({
     el: '#app',
     render: h => h(App),
-    router
 })
